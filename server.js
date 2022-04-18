aaaa62685b0f4ea9b6adb041e96653ccc74404ea
pronazmul <developernazmul@gmail.com>
2022-04-18 22:53:13 +0600

    Standard Project Setup and Node Js Data Parsed

--- server.js
+++ server.js
@@ -1,46 +1,65 @@
-const http = require('http')
-const fs = require('fs')
+/*
+* Title: Request Data Parsed
+* Description: All Request Data Parsed like, Url, QueryParams, Headers, Request Body As Json.
+* Author: Nazmul Huda
+* Date: 18/04/2022
+*/
 
-const server = http.createServer((req, res) => {
+// Dependencies
+const http = require('http');
+const url = require('url');
+const {StringDecoder} = require('string_decoder');
 
-    if(req.url==='/'){
-        res.write(`
-        <html>
-            <head>
-                <title>Write Here</title>   
-            </head>
-        <body>
-        `)
+// Module Scaffolding
+const app = {};
 
-        res.write(`
-            <form action='/process' method='post'>
-                <input type="text" name="data" placeholder="Pass Data Here"/>
-            </form>
-        <body>
-        </html>`)
-        return res.end()
-    }else if(req.url==='/process' && req.method==='POST'){
-        let body = []
-        // Recieve Buffer Chunk by Chunk and push it to an array, Req is a readable stream
-        req.on('data', (chunk) => body.push(chunk))
+//  Configuration
+app.config = {
+    port:5000,
+}
 
-        // When all the data is recieved Make All Buffer Chunks to a String
-        req.on('end', () => {
-            console.log("Data Recived Done")
-            body = Buffer.concat(body).toString()
-
-            res.write(body)
-            return res.end()
-        })
-    }else if(req.url==='/read'){
-        let readStream = fs.createReadStream('./read.txt', 'utf8')
-        //Write Stream as a response, response is a writable stream
-        readStream.pipe(res)
-    }else{
-        res.write('Not Found')
-        return res.end()
-    }
-})
+// Create Server
+app.createServer = (callback)=> {
+    // Create Server
+    const server = http.createServer(callback);
+    // listen To Port
+    server.listen(app.config.port, ()=> {
+        console.log(`Server is listening on port ${app.config.port}`);
+    })
+}
 
 
-server.listen(5000)
+// Handle Request Response
+app.handleRequestResponse = (req, res)=> {
+    // Parse Request URL
+    const parsedUrl = url.parse(req.url, true);
+    // Get Path
+    const path = (parsedUrl.pathname).replace(/^\/+|\/+$/g, '');
+    // Get Query String
+    const queryStringObject = parsedUrl.query;
+    // Get HTTP Method
+    const method = req.method.toLowerCase();
+    // Get Headers
+    const headers = req.headers;
+    // Get Payload & Decode
+    const decoder = new StringDecoder('utf-8');
+    let body = '';
+
+    // Listen Data Event on Request
+    req.on('data', (buffer)=> {
+        body += decoder.write(buffer);
+    })
+    // Listen End Event on Request
+    req.on('end', ()=> {
+        body += decoder.end();
+        // Make Request Body to Json
+        const json = JSON.parse(body.replace(/\r\n/g,''))
+        console.log({json, body, path, queryStringObject, method, headers})
+        res.end("Hello World")
+    })
+}
+
+
+// Start Server
+app.createServer(app.handleRequestResponse);
+
